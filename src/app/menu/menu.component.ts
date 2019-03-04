899878dfae94719215914d45b5d012068e1b13ea
Alejandra Giraldo <m.aleja3000@gmail.com>
2019-03-03 23:30:57 -0500

    Add Menu Resposive And Add Footer

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  showMenu;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  buttonMenu(event) {
+    if (event) {
+      this.showMenu = !this.showMenu;
+    }
+  }
+
+}
